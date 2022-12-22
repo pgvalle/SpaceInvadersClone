edfbe37629b911fd622e12de84b9e23b938aabb5
pgvalle <pgvalle.nogueira@gmail.com>
2022-12-22 17:07:44 -0300

    Refactoring in cannon and implemented shot mechanic update

--- src/entities/Cannon.c
+++ src/entities/Cannon.c
@@ -5,68 +5,81 @@
     struct App* app = GetAppInstance();\
     struct Cannon* cannon = &app->game.cannon;
 
-void Cannon_Initialize()
+void InitCannon()
 {
     struct Cannon* cannon = &GetAppInstance()->game.cannon;
     cannon->x = CANNON_X_INIT;
-    cannon->lives = CANNON_LIVES_TOTAL;
-    cannon->shotTimer = 0;
+
+    cannon->dead = false;
+    cannon->deaths = 0;
     cannon->deathTimer = 0;
     cannon->deathFrame = 0;
     cannon->deathFrameTimer = 0;
-    cannon->wasShot = SDL_FALSE;
-    cannon->shooting = SDL_FALSE;
+
+    cannon->shooting = false;
+    cannon->shotTimer = 0;
 }
 
-void Cannon_Update()
+void UpdateCannon()
 {
     GET_APP_AND_CANNON;
 
-    const Uint8* keys = SDL_GetKeyboardState(NULL);
-    if (keys[SDL_SCANCODE_RETURN])
-        cannon->wasShot = SDL_TRUE;
+    cannon->shooting = false; // assume cannon isn't shooting
 
-    if (cannon->wasShot)
+    if (cannon->dead)
     {
-        cannon->deathTimer += app->frameTime;
-        if (cannon->deathTimer >= CANNON_DEATH_DELTA)
-        {
-            cannon->wasShot = SDL_FALSE;
-            cannon->deathTimer = 0;
-        }
+        // update death animation
         cannon->deathFrameTimer += app->frameTime;
-        if (cannon->deathFrameTimer >= CANNON_DEATH_FRAME_DELTA)
+        if (cannon->deathFrameTimer >= CANNON_SWAP_DEATH_FRAME_TIMEOUT)
         {
             cannon->deathFrame = !cannon->deathFrame;
             cannon->deathFrameTimer = 0;
         }
+        // update death
+        cannon->deathTimer += app->frameTime;
+        if (cannon->deathTimer >= CANNON_DEATH_TIMEOUT)
+        {
+            cannon->deaths++;
+            // reset death timer stuff entirely
+            cannon->deathFrame = 0;
+            cannon->deathTimer = 0;
+            cannon->deathFrameTimer = 0;
+        }
     }
-    else
+    else // not dead
     {
+        // update position with keyboard input
+        GET_APP_KEYS;
         if (keys[SDL_SCANCODE_LEFT])
             cannon->x -= 1;
         if (keys[SDL_SCANCODE_RIGHT])
             cannon->x += 1;
+
+        // update shot mechanic
+        if (cannon->shotTimer < CANNON_SHOT_COOLDOWN)
+            cannon->shotTimer += app->frameTime;
+        else if (keys[SDL_SCANCODE_SPACE])
+        {
+            cannon->shotTimer = 0;
+            cannon->shooting = true;
+        }
     }
 }
 
-void Cannon_Render()
+void RenderCannon()
 {
     GET_APP_AND_CANNON;
 
-    if (!cannon->wasShot)
+    if (!cannon->dead)
     {
         SDL_Rect clip = { 0,0, 16,8 };
         SDL_Rect scale = { APP_SCALE(cannon->x),APP_SCALE(CANNON_Y), APP_SCALE(16),APP_SCALE(8) };
-        SDL_RenderCopy(app->renderer, app->textures[CANNON_TEX_INDEX], &clip, &scale);
+        SDL_RenderCopy(app->renderer, app->textures[TEXINDEX_CANNON], &clip, &scale);
     }
-    else if (cannon->deathTimer < CANNON_DEATH_DELTA)
+    else if (cannon->deathTimer < CANNON_DEATH_TIMEOUT)
     {
-        SDL_Rect clip = {16, 0, 16, 8};
+        SDL_Rect clip = { (cannon->deathFrame ? 32 : 16),0, 16,8 };
         SDL_Rect scale = { APP_SCALE(cannon->x),APP_SCALE(CANNON_Y), APP_SCALE(16),APP_SCALE(8) };
-        if (cannon->deathFrame == 1)
-            clip.x = 32;
-        SDL_RenderCopy(app->renderer, app->textures[CANNON_TEX_INDEX], &clip, &scale);
-        // render death
+        SDL_RenderCopy(app->renderer, app->textures[TEXINDEX_CANNON], &clip, &scale);
     }
 }
--- src/entities/Cannon.h
+++ src/entities/Cannon.h
@@ -1,36 +1,33 @@
 #ifndef CANNON_H_INCLUDED
 #define CANNON_H_INCLUDED
 
-#include <SDL.h>
+#include <stdbool.h>
 
 #define CANNON_X_INIT 26
 #define CANNON_Y      220 // won't change
 
-#define CANNON_MOVEMENT_PACE 1
-
-#define CANNON_MOVE_DELTA        16
-#define CANNON_SHOT_COOLDOWN     1000
-#define CANNON_DEATH_DELTA       3000
-#define CANNON_DEATH_FRAME_DELTA 100
+#define CANNON_SHOT_COOLDOWN            1000
+#define CANNON_DEATH_TIMEOUT            1000
+#define CANNON_SWAP_DEATH_FRAME_TIMEOUT 100
 
 #define CANNON_LIVES_TOTAL 3
 
-#define CANNON_TEX_INDEX 0
-
 struct Cannon
 {
-    int x;
-    int lives;
+    int x; // !!!
+
+    bool dead;  // !!!
+    int deaths; // !!!
     int deathTimer;
     int deathFrame;
     int deathFrameTimer;
+    
+    bool shooting; // !!!
     int shotTimer;
-    SDL_bool wasShot;
-    SDL_bool shooting;
 };
 
-void Cannon_Initialize();
-void Cannon_Update();
-void Cannon_Render();
+void InitCannon();
+void UpdateCannon();
+void RenderCannon();
 
 #endif // CANNON_H_INCLUDED
