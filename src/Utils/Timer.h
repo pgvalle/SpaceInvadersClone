73737b9ea9110877909e0d99d78521c17b3ba52e
pgvalle <pgvalle.nogueira@gmail.com>
2022-12-31 16:20:36 -0300

    Timer.c removed. Implemented UpdateTimer in header

--- src/Utils/Timer.h
+++ src/Utils/Timer.h
@@ -12,7 +12,20 @@ struct Timer
 	Uint32 timeout;
 };
 
-void UpdateTimer(struct Timer* timer);
+void UpdateTimer(struct Timer* timer)
+{
+	// assume timer didn't reach timeout yet
+	timer->reachedTimeout = false;
+
+	// update timer in fact
+	timer->time += app.frameTime;
+	if (timer->time >= timer->timeout)
+	{
+		timer->time = 0;
+		// valid only until next call to UpdateTimer
+		timer->reachedTimeout = true;
+	}
+}
 
 
 #endif // TIMER_H
