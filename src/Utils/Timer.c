24e50c9d66823747491841fab897024e1d87a7c2
pgvalle <pgvalle.nogueira@gmail.com>
2022-12-31 17:20:14 -0300

    Readded Timer.c

---
+++ src/Utils/Timer.c
@@ -0,0 +1,16 @@
+#include "Timer.h"
+
+void UpdateTimer(struct Timer* timer)
+{
+	// assume timer didn't reach timeout yet
+	timer->reachedTimeout = false;
+
+	// update timer in fact
+	timer->time += app.frameTime;
+	if (timer->time >= timer->timeout)
+	{
+		timer->time = 0;
+		// valid only until next call to UpdateTimer
+		timer->reachedTimeout = true;
+	}
+}
--- src/Utils/Timer.h
+++ src/Utils/Timer.h
@@ -12,20 +12,7 @@ struct Timer
 	Uint32 timeout;
 };
 
-void UpdateTimer(struct Timer* timer)
-{
-	// assume timer didn't reach timeout yet
-	timer->reachedTimeout = false;
-
-	// update timer in fact
-	timer->time += app.frameTime;
-	if (timer->time >= timer->timeout)
-	{
-		timer->time = 0;
-		// valid only until next call to UpdateTimer
-		timer->reachedTimeout = true;
-	}
-}
+void UpdateTimer(struct Timer* timer);
 
 
 #endif // TIMER_H
