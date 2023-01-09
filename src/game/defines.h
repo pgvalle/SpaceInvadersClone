495067114d3d09a32384063fbdb4b80310a0db91
pgvalle <pgvalle.nogueira@gmail.com>
2023-01-09 18:21:33 -0300

    Created defines for game stuff

---
+++ src/game/defines.h
@@ -0,0 +1,20 @@
+#ifndef GAME_DEFINES_H
+#define GAME_DEFINES_H
+
+#define RESOURCE_DIR "./res"
+
+#define DEFAULT_SCALE 2
+#define DEFAULT_VOLUME 50
+
+#define FONT_PTSIZE 8
+
+#define CHARACTER_SET_MONO "A B C D E F G H  I J K L M N O P Q R S T "\
+    "U V W X Y Z 0  1 2 3 4 5 6 7 8 9 <  > * ? -  = "
+
+#define CHARACTER_SET "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<>*?-="
+
+#define WORLD_WIDTH  224
+#define WORLD_HEIGHT 256
+
+
+#endif // GAME_DEFINES_H
