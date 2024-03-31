c9ae0ca50ec3256341a72a7a37f502f43cee78ef
pgvalle <pgvalle.nogueira@gmail.com>
2024-03-31 00:37:38 +0000

    std=c++17

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Specify the directory containing your .cpp files
 SOURCE_DIR="src"
 BUILD_DIR="build"
-FLAGS="-O3 -Wall -DNDEBUG -I$SOURCE_DIR $(pkg-config --cflags sdl2 SDL2_image SDL2_ttf)"
+FLAGS="-O3 -Wall -DNDEBUG -std=c++17 -I$SOURCE_DIR $(pkg-config --cflags sdl2 SDL2_image SDL2_ttf)"
 LIBS="$(pkg-config --libs sdl2 SDL2_image SDL2_ttf)"
 
 find_src_files() {
