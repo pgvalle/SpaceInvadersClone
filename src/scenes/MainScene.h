1d9354351eac9ad0b2f8f2d74f38e1dcb00a5a9d
pgvalle <pgvalle.nogueira@gmail.com>
2024-05-04 11:37:13 +0000

    MainScene responsible for globals

--- src/scenes/MainScene.h
+++ src/scenes/MainScene.h
@@ -4,12 +4,13 @@
 
 class MainScene : public Scene {
 private:
+  int coins, score, hiScore;
 
 public:
   MainScene();
   ~MainScene();
 
-  //bool processEvent(const SDL_Event &event) override;
+  void processEvent(const SDL_Event &event) override;
   void update(Uint32 delta) override;
   void render(SDL_Renderer *renderer) override;
 };
