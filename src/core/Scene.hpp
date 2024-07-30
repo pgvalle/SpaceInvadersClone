1063485413be604696ec1169b3c9aca1a79f92f2
pgvalle <pgvalle.nogueira@gmail.com>
2024-07-30 10:01:29 -0300

    process event receives engine reference and event reference

--- src/core/Scene.hpp
+++ src/core/Scene.hpp
@@ -5,9 +5,9 @@ class Engine;
 
 class Scene {
 public:
-  virtual void process_events(Engine &eng);
-  virtual void update(Engine &eng);
-  virtual void render(const Engine &eng);
+  virtual void process_event(Engine *eng, const SDL_Event &evt);
+  virtual void update(Engine *eng);
+  virtual void render(const Engine *eng);
 };
 
 #endif  // SCENE
