f2cf9774b29c184af5eace67ad18e7f0c4c7ec69
pgvalle <pgvalle.nogueira@gmail.com>
2024-08-06 18:26:11 -0300

    defining scene

--- src/core/Scene.hpp
+++ src/core/Scene.hpp
@@ -1,13 +1,17 @@
 #ifndef SCENE
 #define SCENE
 
+#include <SDL2/SDL.h>
+
 class Engine;
 
 class Scene {
 public:
-  virtual void process_event(Engine *eng, const SDL_Event &evt);
-  virtual void update(Engine *eng);
-  virtual void render(const Engine *eng);
+  virtual ~Scene() {}
+
+  virtual void process_event(Engine *eng, const SDL_Event &evt) = 0;
+  virtual void update(Engine *eng) = 0;
+  virtual void render(const Engine *eng) = 0;
 };
 
 #endif  // SCENE
