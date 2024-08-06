af31c6695cd3eb78f6232a212b991a631a63096e
pgvalle <pgvalle.nogueira@gmail.com>
2024-08-06 18:26:00 -0300

    common header includes core

---
+++ src/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON
+#define COMMON
+
+#include <cstdio>
+#include <cassert>
+
+#include <core/defines.h>
+#include <core/Scene.hpp>
+#include <core/Engine.hpp>
+#include <util/Timer.hpp>
+
+#endif  // COMMON
